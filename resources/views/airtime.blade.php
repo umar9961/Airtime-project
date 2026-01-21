42682d387631d1e7d1483d0caf3bc331c2bddb05
Umar <farukmultilink1490@gmail.com>
2026-01-21 11:01:46 +0100

    Airtime and data page

--- resources/views/airtime.blade.php
+++ resources/views/airtime.blade.php
@@ -1,60 +1,54 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Airtime & Data VTU</title>
+    <title>Airtime & Data</title>
 </head>
 <body>
 
 <h2>Buy Airtime</h2>
 
-<form method="POST" action="/buy-airtime">
-    @csrf
-
+<form>
     <label>Network</label><br>
-    <select name="network" required>
-        <option value="">Select Network</option>
-        <option value="MTN">MTN</option>
-        <option value="AIRTEL">Airtel</option>
-        <option value="GLO">Glo</option>
-        <option value="9MOBILE">9mobile</option>
+    <select>
+        <option>MTN</option>
+        <option>Airtel</option>
+        <option>Glo</option>
+        <option>9mobile</option>
     </select><br><br>
 
     <label>Phone Number</label><br>
-    <input type="text" name="phone" placeholder="080xxxxxxxx" required><br><br>
+    <input type="text" placeholder="080xxxxxxxx"><br><br>
 
     <label>Amount (₦)</label><br>
-    <input type="number" name="amount" required><br><br>
+    <input type="number"><br><br>
 
-    <button type="submit">Buy Airtime</button>
+    <button>Buy Airtime</button>
 </form>
 
 <hr>
 
 <h2>Buy Data</h2>
 
-<form method="POST" action="/buy-data">
-    @csrf
-
+<form>
     <label>Network</label><br>
-    <select name="network" required>
-        <option value="">Select Network</option>
-        <option value="MTN">MTN</option>
-        <option value="AIRTEL">Airtel</option>
-        <option value="GLO">Glo</option>
-        <option value="9MOBILE">9mobile</option>
+    <select>
+        <option>MTN</option>
+        <option>Airtel</option>
+        <option>Glo</option>
+        <option>9mobile</option>
+    </select><br><br>
+
+    <label>Data Plan</label><br>
+    <select>
+        <option>500MB</option>
+        <option>1GB</option>
+        <option>2GB</option>
     </select><br><br>
 
     <label>Phone Number</label><br>
-    <input type="text" name="phone" placeholder="080xxxxxxxx" required><br><br>
+    <input type="text" placeholder="080xxxxxxxx"><br><br>
 
-    <label>Data Plan</label><br>
-    <select name="plan" required>
-        <option value="500MB">500MB</option>
-        <option value="1GB">1GB</option>
-        <option value="2GB">2GB</option>
-    </select><br><br>
-
-    <button type="submit">Buy Data</button>
+    <button>Buy Data</button>
 </form>
 
 </body>
