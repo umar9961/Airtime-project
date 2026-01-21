359d52ab8b93d28e64c91d8ef66fded81f9bbd1f
Umar <farukmultilink1490@gmail.com>
2026-01-21 21:57:57 +0100

    Fix airtime page

--- resources/views/airtime.blade.php
+++ resources/views/airtime.blade.php
@@ -7,7 +7,7 @@
 
 <h3>Your Wallet Balance</h3>
 <p>
-    ₦{{ number_format(optional(\App\Models\Wallet::first())->balance ?? 0, 2) }}
+₦{{ number_format(optional(\App\Models\Wallet::first())->balance ?? 0) }}
 </p>
 
 <hr>
@@ -24,16 +24,13 @@
         <option value="Airtel">Airtel</option>
         <option value="Glo">Glo</option>
         <option value="9mobile">9mobile</option>
-    </select>
-    <br><br>
+    </select><br><br>
 
     <label>Phone Number</label><br>
-    <input type="text" name="phone" placeholder="080xxxxxxxx" required>
-    <br><br>
+    <input type="text" name="phone" placeholder="080xxxxxxxx" required><br><br>
 
     <label>Amount (₦)</label><br>
-    <input type="number" name="amount" required>
-    <br><br>
+    <input type="number" name="amount" required><br><br>
 
     <button type="submit">Buy Airtime</button>
 </form>
