04383acc6604715ac6217776e59b4a978871f3a5
Umar <farukmultilink1490@gmail.com>
2026-01-21 21:30:07 +0100

    Fix homepage route to airtime page

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaymentController;
 
 Route::get('/', function () {
     return view('airtime');
 });
 
-Route::post('/pay', [App\Http\Controllers\PaymentController::class, 'pay']);
-Route::get('/payment/success', [App\Http\Controllers\PaymentController::class, 'success']);
-use App\Models\Wallet;
-
-Route::get('/wallet', function () {
-    $wallet = Wallet::firstOrCreate(
-        ['user_id' => 1],
-        ['balance' => 0]
-    );
-
-    return "Wallet Balance: ₦" . number_format($wallet->balance, 2);
-});
+Route::post('/pay', [PaymentController::class, 'pay']);
