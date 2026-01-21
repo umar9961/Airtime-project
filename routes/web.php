9b6ddfaff1046658b63bbd59a171d1b721f29ebe
Umar <farukmultilink1490@gmail.com>
2026-01-21 21:45:53 +0100

    Fix duplicate airtime form

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PaymentController;
 
 Route::get('/', function () {
     return view('airtime');
 });
 
-Route::post('/pay', [PaymentController::class, 'pay']);
+Route::post('/pay', [\App\Http\Controllers\PaymentController::class, 'pay']);
